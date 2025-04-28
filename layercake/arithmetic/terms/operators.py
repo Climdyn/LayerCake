d863779c8f6e95f197d749a3682f51159a617884
Jonathan Demaeyer <jodemaey@meteo.be>
2025-04-28 19:56:31 +0200

    Coded operators composition term (still needs tests)

--- layercake/arithmetic/terms/operators.py
+++ layercake/arithmetic/terms/operators.py
@@ -45,3 +45,60 @@ class OperatorTerm(ArithmeticTerm):
                          for j in range(nmod)]
 
         return args_list
+
+
+class ComposedOperatorsTerm(ArithmeticTerm):
+
+    def __init__(self, field, inner_product_definition, operator1, operator_args1,
+                 operator2, operator_args2, parameter=None, name=''):
+
+        ArithmeticTerm.__init__(self, field, inner_product_definition, name)
+        self._rank = 1
+        self.parameter = parameter
+        if isinstance(operator_args1, list):
+            operator_args1 = tuple(operator_args1)
+        elif not isinstance(operator_args1, tuple):
+            operator_args1 = tuple([operator_args1])
+        self._operator1 = operator1(*operator_args1)
+        if isinstance(operator_args2, list):
+            operator_args2 = tuple(operator_args2)
+        elif not isinstance(operator_args2, tuple):
+            operator_args2 = tuple([operator_args2])
+        self._operator2 = operator1(*operator_args2)
+
+    @property
+    def symbolic_expression(self):
+        if self.parameter is None:
+            return Mul(self._operator1,
+                       Mul(self._operator2, self.field.symbol, evaluate=False), evaluate=False)
+        else:
+            return Mul(self.parameter.symbol,
+                       Mul(self._operator1,
+                           Mul(self._operator2, self.field.symbol, evaluate=False), evaluate=False),
+                       evaluate=False)
+
+    @property
+    def numerical_expression(self):
+        if self.parameter is None:
+            return Mul(self._operator1,
+                       Mul(self._operator2, self.field.symbol, evaluate=False), evaluate=False)
+        else:
+            return Mul(self.parameter,
+                       Mul(self._operator1,
+                           Mul(self._operator2, self.field.symbol, evaluate=False), evaluate=False),
+                       evaluate=False)
+
+    def _integrations(self, basis, numerical=False):
+        nmod = len(basis)
+        if numerical:
+            args_list = [[(i, j), self.inner_product_definition.inner_product,
+                          (basis[i], self._evaluate(self.numerical_function(disable_commutativity(basis[j]))))]
+                         for i in range(nmod)
+                         for j in range(nmod)]
+        else:
+            args_list = [[(i, j), self.inner_product_definition.inner_product,
+                          (basis[i], self._evaluate(self.symbolic_function(disable_commutativity(basis[j]))))]
+                         for i in range(nmod)
+                         for j in range(nmod)]
+
+        return args_list
