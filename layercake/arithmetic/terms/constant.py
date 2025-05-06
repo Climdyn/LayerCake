b2120596440beee98d5ab91218b69a13d3a39732
Jonathan Demaeyer <jodemaey@meteo.be>
2025-05-06 14:46:35 +0200

    ConstantTerm class now derives from base class

--- layercake/arithmetic/terms/constant.py
+++ layercake/arithmetic/terms/constant.py
@@ -1,19 +1,50 @@
-import numpy as np
 
-from layercake.arithmetic.terms.base import SingleArithmeticTerm
+from layercake.arithmetic.terms.base import ArithmeticTerm
 from layercake.variables.field import ParameterField
 
+from layercake.utils.commutativity import disable_commutativity
+from sympy import Lambda
 
-class ConstantTerm(SingleArithmeticTerm):
 
-    def __init__(self, parameters_field, inner_product_definition=None, name=''):
+class ConstantTerm(ArithmeticTerm):
+
+    def __init__(self, parameters_field, name=''):
 
         if not isinstance(parameters_field, ParameterField):
             raise ValueError('Input field for constant field must be a ParameterField object.')
 
-        SingleArithmeticTerm.__init__(self, parameters_field, inner_product_definition, None, name)
+        ArithmeticTerm.__init__(self, name)
 
         self._rank = 1
+        self.field = parameters_field
+
+    @property
+    def _symbolic_expressions_list(self):
+        return [self.symbolic_expression]
+
+    @property
+    def _numerical_expressions_list(self):
+        return [self.numerical_expression]
+
+    @property
+    def _symbolic_functions_list(self):
+        return [self.symbolic_function]
+
+    @property
+    def _numerical_functions_list(self):
+        return [self.numerical_function]
+
+    @property
+    def symbolic_function(self):
+        foo = disable_commutativity(self.symbolic_expression)
+        ss = disable_commutativity(self.field.symbol)
+        return Lambda(ss, foo)
+
+    @property
+    def numerical_function(self):
+        foo = disable_commutativity(self.numerical_expression)
+        ss = disable_commutativity(self.field.symbol)
+        return Lambda(ss, foo)
 
     @property
     def symbolic_expression(self):
@@ -22,6 +53,9 @@ class ConstantTerm(SingleArithmeticTerm):
     @property
     def numerical_expression(self):
         return self.symbolic_expression
+
+    def _inner_product_arguments(self, basis, indices, numerical=False):
+        pass
 
     def compute_inner_products(self, basis, numerical=False, timeout=None, num_threads=None, permute=False):
         pass
