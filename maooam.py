094e4d77e4c564c293f8819ca62e8612f107c6ba
Jonathan Demaeyer <jodemaey@meteo.be>
2025-06-11 16:09:16 +0200

    First test of the MAOOAM example

--- maooam.py
+++ maooam.py
@@ -20,80 +20,64 @@ from layercake.bakery.cake import Cake
 ##########################
 
 # Characteristic length scale (L_y / pi)
-L_symbol = Symbol('L')
-L = Parameter(1591549.4309189534, symbol=L_symbol, units='[m]')
+L = Parameter(1591549.4309189534, symbol=Symbol('L'), units='[m]')
 
 # Domain aspect ratio
-n_symbol = symbols('n')
-n = Parameter(1.5, symbol=n_symbol)
+n = Parameter(1.5, symbol=symbols('n'))
 
 # Coriolis parameter at the middle of the domain
-f0_symbol = Symbol('f_0')
-f0 = Parameter(1.032e-4, symbol=f0_symbol, units='[s^-1]')
+f0 = Parameter(1.032e-4, symbol=Symbol('f_0'), units='[s^-1]')
 
 # Pressure difference between the two atmospheric layers
-deltap_symbol = Symbol('Δp')
-deltap = Parameter(5.e4, symbol=deltap_symbol, units='[Pa]')
+deltap = Parameter(5.e4, symbol=Symbol('Δp'), units='[Pa]')
 
 # Static stability of the atmosphere
-sigma_symbol = symbols('σ')
-sigma = Parameter(2.1581898457499433e-06, symbol=sigma_symbol, units='[m^2][s^-2][Pa^-2]')
+sigma = Parameter(2.1581898457499433e-06, symbol=Symbol('σ'), units='[m^2][s^-2][Pa^-2]')
 
 # Meridional gradient of the Coriolis parameter at phi_0
-beta_symbol = Symbol(u'β')
-beta = Parameter(1.3594204385792041e-11, symbol=beta_symbol, units='[m^-1][s^-1]')
+beta = Parameter(1.3594204385792041e-11, symbol=Symbol(u'β'), units='[m^-1][s^-1]')
 
 # atmosphere bottom friction coefficient
-kd_symbol = Symbol('k_d')
-kd = Parameter(1.032e-05, symbol=kd_symbol, units='[s^-1]')
+kd = Parameter(1.032e-05, symbol=Symbol('k_d'), units='[s^-1]')
 
 # Atmosphere internal friction coefficient
-kdp_symbol = Symbol("k_d'")
-kdp = Parameter(1.032e-06, symbol=kdp_symbol, units='[s^-1]')
+kdp = Parameter(1.032e-06, symbol=Symbol("k_d'"), units='[s^-1]')
 
 # Friction between the ocean and the atmosphere
-d_symbol = symbols('d')
-d = Parameter(0.05, symbol=d_symbol)
+d = Parameter(0.05, symbol=Symbol('d'))
 
 # Friction of the ocean with the bottom
-r_symbol = symbols('r')
-r = Parameter(1.e-7, symbol=r_symbol, units='[s^-1]')
+r = Parameter(1.e-7, symbol=Symbol('r'), units='[s^-1]')
 
 # Physical constants
 # gas constant of dry air
-rr_symbol = Symbol('R')
-rr = Parameter(287.058, symbol=rr_symbol, units='[J][kg^-1][K^-1]')
+rr = Parameter(287.058, symbol=Symbol('R'), units='[J][kg^-1][K^-1]')
 # Stefan-Boltzmann constant
-sb_symbol = Symbol('σ_B')
-sb = Parameter(5.67e-08, symbol=sb_symbol, units='[J][m^-2][s^-1][K^-4]')
+sb = Parameter(5.67e-08, symbol=Symbol('σ_B'), units='[J][m^-2][s^-1][K^-4]')
 
 # Sensible + turbulent heat exchange between ocean/ground and atmosphere
-hlambda_symbol = Symbol('λ')
-hlambda = Parameter(15.06, symbol=hlambda_symbol, units='[W][m^-2][K^-1]')
+hlambda = Parameter(15.06, symbol=Symbol('λ'), units='[J][m^-2][K^-1][s^-1]')
 
 # Stationary solution for the 0-th order atmospheric temperature
-T0a_symbol = Symbol('T_{a, 0}')
-T0a = Parameter(289.3, symbol=T0a_symbol, units='[K]')
+T0a = Parameter(289.3, symbol=Symbol('T_{a, 0}'), units='[K]')
 
 # Stationary solution for the 0-th order atmospheric temperature
-T0o_symbol = Symbol('T_{o, 0}')
-T0o = Parameter(301.46, symbol=T0o_symbol, units='[K]')
+T0o = Parameter(301.46, symbol=Symbol('T_{o, 0}'), units='[K]')
 
 # Specific heat capacity of the ocean
-gamma_o_symbol = Symbol('γ_o')
-gamma_o = Parameter(560000000.0, symbol=gamma_o_symbol, units='[J][m^-2][K^-1]')
+gamma_o = Parameter(560000000.0, symbol=Symbol('γ_o'), units='[J][m^-2][K^-1]')
 
 # Specific heat capacity of the atmosphere
-gamma_a_symbol = Symbol('γ_a')
-gamma_a = Parameter(10000000.0, symbol=gamma_a_symbol, units='[J][m^-2][K^-1]')
+gamma_a = Parameter(10000000.0, symbol=Symbol('γ_a'), units='[J][m^-2][K^-1]')
 
 # Depth of the water layer of the ocean
-h_symbol = Symbol('h')
-h = Parameter(136.5, symbol=h_symbol, units='[m]')
+h = Parameter(136.5, symbol=Symbol('h'), units='[m]')
 
 # Emissivity coefficient for the grey-body atmosphere
-eps_symbol = Symbol('ε')
-eps = Parameter(0.7, symbol=eps_symbol)
+eps = Parameter(0.7, symbol=Symbol('ε'))
+
+# Reduced gravity
+gp = Parameter(0.031, symbol=Symbol("g'"), units='[m][s^-2]')
 
 # Defining the domain
 ######################
@@ -101,7 +85,7 @@ eps = Parameter(0.7, symbol=eps_symbol)
 parameters = {'n': n}
 atmospheric_basis = contiguous_channel_basis(2, 2, parameters)
 oceanic_basis = contiguous_basin_basis(2, 2, parameters)
-s = StandardSymbolicInnerProductDefinition(coordinate_system=b.coordinate_system)
+s = StandardSymbolicInnerProductDefinition(coordinate_system=atmospheric_basis.coordinate_system)
 # coordinates
 x = atmospheric_basis.coordinate_system.coordinates_symbol_as_list[0]
 y = atmospheric_basis.coordinate_system.coordinates_symbol_as_list[1]
@@ -109,12 +93,30 @@ y = atmospheric_basis.coordinate_system.coordinates_symbol_as_list[1]
 # Derived (non-dimensional) parameters
 #######################################
 
-sigma_nondim = Parameter((sigma * deltap ** 2) / (L ** 2 * f0 ** 2), symbol=sigma_symbol, units='')
-beta_nondim = Parameter(beta * L / f0, symbol=beta_symbol, units='')
-a_symbol = symbols('a')
-a = Parameter(2 / sigma_nondim, symbol=a_symbol)
-kd_deriv = Parameter(0.5 * kd / f0, symbol=kd_symbol)
-kdp_deriv = Parameter(2 * kdp / f0, symbol=kdp_symbol)
+sigma_nondim = Parameter((sigma * deltap ** 2) / (L ** 2 * f0 ** 2), symbol=sigma.symbol, units='')
+beta_nondim = Parameter(beta * L / f0, symbol=beta.symbol, units='')
+a = Parameter(2 / sigma_nondim, symbol=Symbol('a'))
+kd_deriv = Parameter(0.5 * kd / f0, symbol=kd.symbol)
+kdp_deriv = Parameter(2 * kdp / f0, symbol=kdp.symbol)
+LR = Parameter((gp * h) ** 0.5 / f0, symbol=Symbol('L_R'), units='[m]')
+G = Parameter(- L ** 2 / LR ** 2, symbol=Symbol('G'))
+Lpo = hlambda / (gamma_o * f0)
+Lpa = hlambda / (gamma_a * f0)
+sbpo = 4 * sb * T0o ** 3 / (gamma_o * f0)
+sbpa = 8 * eps * sb * T0a ** 3 / (gamma_o * f0)
+LSBpo = 2 * eps * sb * T0o ** 3 / (gamma_a * f0)
+LSBpa = 8 * eps * sb * T0a ** 3 / (gamma_a * f0)
+
+# Defining the insolation
+# In the ocean
+Cov = np.zeros(len(atmospheric_basis))
+Cov[0] = 310
+Cpo = ParameterField("Co'", Symbol("Co'"), Cov / (gamma_o * f0) * rr / (f0 ** 2 * L ** 2), atmospheric_basis, s)
+
+# In the atmosphere
+Cav = np.zeros(len(atmospheric_basis))
+Cav[0] = 310./3.
+Cpa = ParameterField("Ca'", Symbol("Ca'"), a * Cav / (gamma_a * f0) * rr / (f0 ** 2 * L ** 2) / 2, atmospheric_basis, s)
 
 # Defining the fields
 #######################
@@ -201,32 +203,25 @@ ground_friction = OperatorTerm(theta_a, Laplacian, atmospheric_basis.coordinate_
 baroclinic_equation.add_rhs_term(ground_friction)
 
 # adding jacobian from thermal wind relation
-
 thermal = Jacobian(psi_a, theta_a, atmospheric_basis.coordinate_system, prefactors=(a, a))
 baroclinic_equation.add_rhs_terms(thermal)
 
 # adding heat exchange scheme
 
 la1 = Symbol('λ1')
-la1p = Parameter(, symbol=la1)
+la1p = Parameter(a * (Lpa + LSBpa), symbol=la1)
 
 la1t = LinearTerm(theta_a, prefactor=la1p, sign=1)
 
 la2 = Symbol('λ2')
-la2p = Parameter(, symbol=la2)
+la2p = Parameter(a * (0.5 * Lpa + LSBpo), symbol=la2)
 
 la2t = LinearTerm(deltaT_o, prefactor=la2p, sign=-1)
 
 baroclinic_equation.add_rhs_terms((la1t, la2t))
 
 # adding the insolation
-
-Cas = Symbol('Ca')
-Cav = np.zeros(len(atmospheric_basis))
-Cav[0] = 0.1
-Ca = ParameterField('Ca', Cas, Cav, atmospheric_basis, s)
-Cat = LinearTerm(Ca, sign=-1)
-
+Cat = LinearTerm(Cpa, sign=-1)
 baroclinic_equation.add_rhs_term(Cat)
 
 
@@ -277,7 +272,6 @@ oceanic_equation.add_rhs_term(bfriction)
 
 # Defining the equation and LHS
 # Laplacian
-
 lhs = LinearTerm(deltaT_o)
 ocean_temperature_equation = Equation(theta_a, lhs_term=lhs)
 
@@ -287,25 +281,19 @@ ocean_temperature_equation.add_rhs_terms(advection_term)
 
 # adding heat exchange scheme
 lo3 = Symbol('λ3')
-lo3p = Parameter(, symbol=lo3)
+lo3p = Parameter(Lpo + sbpo, symbol=lo3)
 
 lo3t = LinearTerm(deltaT_o, prefactor=lo3p, sign=-1)
 
 lo4 = Symbol('λ4')
-lo4p = Parameter(, symbol=lo4)
+lo4p = Parameter(2 * Lpo + sbpa, symbol=lo4)
 
 lo4t = LinearTerm(theta_a, prefactor=lo4p, sign=1)
 
 ocean_temperature_equation.add_rhs_terms((lo3t, lo4t))
 
 # adding the insolation
-
-Cos = Symbol('Co')
-Cov = np.zeros(len(atmospheric_basis))
-Cov[0] = 0.1
-Co = ParameterField('Co', Cos, Cov, atmospheric_basis, s)
-Cot = LinearTerm(Co, sign=-1)
-
+Cot = LinearTerm(Cpo, sign=1)
 ocean_temperature_equation.add_rhs_term(Cot)
 
 # --------------------------------
@@ -318,6 +306,9 @@ atmospheric_layer = Layer()
 atmospheric_layer.add_equation(barotropic_equation)
 atmospheric_layer.add_equation(baroclinic_equation)
 
+oceanic_layer = Layer()
+oceanic_layer.add_equation(oceanic_equation)
+oceanic_layer.add_equation(ocean_temperature_equation)
 
 # --------------------------------
 #
@@ -327,6 +318,8 @@ atmospheric_layer.add_equation(baroclinic_equation)
 
 cake = Cake()
 cake.add_layer(atmospheric_layer)
+cake.add_layer(oceanic_layer)
+
 
 # --------------------------------
 #
@@ -350,4 +343,4 @@ res = solve_ivp(f, (0., 20000.), ic)
 # plotting
 plt.plot(res.y.T)
 plt.show()
-
+#
