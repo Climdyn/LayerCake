ac4a9035e617b627509900d67a803d4ad4b819c2
Jonathan Demaeyer <jodemaey@meteo.be>
2025-10-09 17:33:45 +0200

    Finished documentation of the equation module

--- layercake/arithmetic/equation.py
+++ layercake/arithmetic/equation.py
@@ -1,10 +1,39 @@
 
+"""
+
+    Main class to define partial differential equation in LayerCake
+    ===============================================================
+
+    This class is the workhorse of LayerCake to define and specify
+    partial differential equation.
+
+"""
 
 from sympy import Symbol, S, Eq
 from layercake.arithmetic.terms.base import ArithmeticTerms, OperationOnTerms
 
 
 class Equation(object):
+    """Main class to define and specify partial differential equations.
+
+    Parameters
+    ----------
+    field: ~field.Field
+        The spatial field over which the partial differential equation.
+    lhs_term: ~arithmetic.terms.base.ArithmeticTerms
+        Term on the left-hand side of the equation.
+        Must be a single term, possibly a combination
+        through :class:`~arithmetic.terms.base.OperationOnTerms` operations.
+
+    Attributes
+    ----------
+    field: ~field.Field
+        The spatial field over which the partial differential equation.
+    terms: list(~arithmetic.terms.base.ArithmeticTerms)
+        List of additive terms in the right-hand side of the equation.
+    lhs_term: ~arithmetic.terms.base.ArithmeticTerms
+        Term on the left-hand side of the equation.
+    """
 
     _t = Symbol('t')
 
@@ -19,16 +48,31 @@ class Equation(object):
         self._cake = None
 
     def add_rhs_term(self, term):
+        """Add a term to the right-hand side of the equation.
+
+        Parameters
+        ----------
+        term: ~arithmetic.terms.base.ArithmeticTerms
+            Term to be added to the right-hand side of the equation.
+        """
         if not issubclass(term.__class__, ArithmeticTerms):
             raise ValueError('Provided term must be a valid ArithmeticTerm object.')
         self.terms.append(term)
 
     def add_rhs_terms(self, terms):
+        """Add multiple terms to the right-hand side of the equation.
+
+        Parameters
+        ----------
+        terms: list(~arithmetic.terms.base.ArithmeticTerms)
+            Terms to be added to the right-hand side of the equation.
+        """
         for t in terms:
             self.add_rhs_term(t)
 
     @property
     def other_fields(self):
+        """list(~field.Field): List of additional fields present in the equation."""
         other_fields = list()
         for equation_term in self.terms:
             for term in equation_term.terms:
@@ -38,6 +82,7 @@ class Equation(object):
 
     @property
     def parameter_fields(self):
+        """list(~field.ParameterField): List of non-dynamical parameter fields present in the equation."""
         parameter_fields = list()
         for equation_term in self.terms:
             for term in equation_term.terms:
@@ -50,6 +95,7 @@ class Equation(object):
 
     @property
     def parameters(self):
+        """list(~field.Parameter): List of parameters present in the equation."""
         parameters_list = list()
         for term in self.terms + [self.lhs_term]:
             if issubclass(term.__class__, OperationOnTerms):
@@ -80,10 +126,12 @@ class Equation(object):
 
     @property
     def parameters_symbols(self):
+        """list(~sympy.core.symbol.Symbol): List of parameter's symbols present in the equation."""
         return [p.symbol for p in self.parameters]
 
     @property
     def symbolic_expression(self):
+        """~sympy.core.expr.Expr: Symbolic expression of the equation."""
         rterm = S.Zero
         for term in self.terms:
             rterm += term.symbolic_expression
@@ -91,6 +139,8 @@ class Equation(object):
 
     @property
     def numerical_expression(self):
+        """~sympy.core.expr.Expr: Expression of the equation with parameters replaced by their
+        configured values."""
         rterm = S.Zero
         for term in self.terms:
             rterm += term.numerical_expression
@@ -98,6 +148,7 @@ class Equation(object):
 
     @property
     def symbolic_rhs(self):
+        """~sympy.core.expr.Expr: Symbolic expression of the right-hand side of the equation."""
         rterm = S.Zero
         for term in self.terms:
             rterm += term.symbolic_expression
@@ -105,6 +156,8 @@ class Equation(object):
 
     @property
     def numerical_rhs(self):
+        """~sympy.core.expr.Expr: Expression of the right-hand side of the equation with
+        parameters replaced by their configured values."""
         rterm = S.Zero
         for term in self.terms:
             rterm += term.numerical_expression
@@ -112,24 +165,53 @@ class Equation(object):
 
     @property
     def symbolic_lhs(self):
+        """~sympy.core.expr.Expr: Symbolic expression of the left-hand side of the equation."""
         return self.lhs_term.symbolic_expression
 
     @property
     def numerical_lhs(self):
+        """~sympy.core.expr.Expr: Expression of the left-hand side of the equation with
+        parameters replaced by their configured values."""
         return self.lhs_term.numerical_expression
 
     @property
     def lhs_inner_products(self):
+        """~sympy.matrices.immutable.ImmutableSparseMatrix or ~sympy.tensor.array.ImmutableSparseNDimArray or sparse.COO(float): Left-hand
+        side inner products of the equation, if available."""
         return self.lhs_term.inner_products
 
     @property
     def maximum_rank(self):
+        """int: Maximum rank of the right-hand side terms tensors."""
         rhs_max_rank = 0
         for term in self.terms:
             rhs_max_rank = max(rhs_max_rank, term.rank)
         return max(rhs_max_rank, self.lhs_term.rank)
 
     def compute_lhs_inner_products(self, basis, numerical=False, timeout=None, num_threads=None, permute=False):
+        """Compute the inner products tensor of the left-hand side term.
+
+        Parameters
+        ----------
+        basis: SymbolicBasis
+            Basis with which to compute the inner products.
+        numerical: bool, optional
+            Whether the resulting computed inner products must be numerical or symbolic.
+            Default to `False`, i.e. symbolic output.
+        num_threads: int or None, optional
+            Number of threads to use to compute the inner products. If `None` use all the cpus available.
+            Default to `None`.
+        timeout: int or float or bool or None, optional
+            The timeout for the numerical computation of each inner product.
+            After the timeout, compute the inner product with a quadrature instead of symbolic integration.
+            Does not apply to symbolic output computations.
+            If `None` or `False`, no timeout occurs.
+            Default to `None`.
+        permute: bool, optional
+            If `True`, applies all the possible permutations to the tensor indices
+            from 1 to the rank of the tensor.
+            Default to `False`, i.e. no permutation is applied.
+        """
         self.lhs_term.compute_inner_products(basis, numerical, timeout, num_threads, permute)
 
     def __repr__(self):
