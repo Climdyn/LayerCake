4cda7e8daaffcee34636566618c3c16928a9d73e
Jonathan Demaeyer <jodemaey@meteo.be>
2025-11-15 10:00:37 +0100

    Adding modules init files

--- layercake/__init__.py
+++ layercake/__init__.py
@@ -2,11 +2,8 @@
 from .variables.parameter import Parameter
 from .variables.field import Field, ParameterField
 from .basis.base import SymbolicBasis
-from .arithmetic.terms.jacobian import vorticity_advection, Jacobian
-from .arithmetic.terms.operators import OperatorTerm
-from .arithmetic.terms.operations import AdditionOfTerms
-from .arithmetic.terms.linear import LinearTerm
-from .arithmetic.terms.constant import ConstantTerm
+from .arithmetic.terms import (vorticity_advection, Jacobian, OperatorTerm,
+                               AdditionOfTerms, LinearTerm, ConstantTerm)
 from .arithmetic.equation import Equation
 from .arithmetic.symbolic.operators import Laplacian, D
 from .bakery.layers import Layer
