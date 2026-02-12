c3a60ff1904f4f25aaa57dabff2d84ee7de1d0c8
Jonathan Demaeyer <jodemaey@meteo.be>
2026-02-12 17:20:55 +0100

    Implementing scalar product of gradients terms (#34)
    * Correcting some bugs in the jacobian module
    
    * Adding code for scalar products of gradients

--- layercake/arithmetic/terms/gradient.py
+++ layercake/arithmetic/terms/gradient.py
@@ -1,12 +1,12 @@
 
 """
 
-    Jacobian and vorticity module
-    =============================
+    Gradient module
+    ===============
 
-    This module defines a function to compute the terms for the Jacobian of fields
+    This module defines a function to compute the terms for the scalar product of gradients of fields
     in partial differential equations.
-    A function to compute derived forms of the Jacobian like the vorticity advection is
+    A function to compute derived forms of the gradient products involving the vorticity is also
     provided.
 
 """
@@ -17,11 +17,11 @@ from layercake.arithmetic.symbolic.expressions import Expression
 from layercake.variables.utils import combine_units, power_units
 
 
-def Jacobian(field1, field2, coordinate_system, sign=1, prefactors=(None, None)):
+def gradients_product(field1, field2, coordinate_system, sign=1, prefactors=(None, None)):
     """Function returning a list of :class:`~layercake.arithmetic.terms.operations.ProductOfTerms` components of
-    the partial differential equation's Jacobian:
+    the partial differential equation's scalar product of gradients:
 
-    .. math:: J(\\psi, \\phi) = e_1 \\, e_2 \\, \\left(\\partial_{u_1} \\psi \\, \\partial_{u_2} \\phi - \\partial_{u_1} \\phi \\, \\partial_{u_2} \\psi \\right)
+    .. math:: P(\\psi, \\phi) = e_1^2 \\, \\partial_{u_1} \\psi \\, \\partial_{u_1} \\phi + e_2^2 \\, \\partial_{u_2} \\psi \\, \\partial_{u_2} \\phi
 
     where :math:`\\phi` and :math:`\\psi` are two fields defined on the model's domain,
     :math:`u_1, u_2` are the coordinates of the model, and :math:`e_1, e_2` are the inverse of the infinitesimal
@@ -30,23 +30,23 @@ def Jacobian(field1, field2, coordinate_system, sign=1, prefactors=(None, None))
     Parameters
     ----------
     field1: ~field.Field or ~field.ParameterField
-        First field on which the Jacobian act (corresponds to the field :math:`\\psi` in the formula above).
+        Field on which the first gradient of the scalar product act (corresponds to the field :math:`\\psi` in the formula above).
     field2: ~field.Field or ~field.ParameterField
-        Second field on which the Jacobian act (corresponds to the field :math:`\\phi` in the formula above).
+        Field on which the second gradient of the scalar product act (corresponds to the field :math:`\\phi` in the formula above).
     coordinate_system: ~systems.CoordinateSystem
         Coordinate system on which the model is defined.
     sign: int, optional
-        Sign in front of the Jacobian term. Either +1 or -1.
+        Sign in front of the term. Either +1 or -1.
         Default to +1.
     prefactors: tuple(~parameter.Parameter or ~expressions.Expression), optional
-        2-tuple providing the prefactors in front of each of the two terms composing the Jacobian.
+        2-tuple providing the prefactors in front of each of the two terms composing the scalar product of gradients.
         This is added on top of the infinitesimal length prefactor.
         Must be specified as model parameters or symbolic expressions.
 
     Returns
     -------
     tuple(~operations.ProductOfTerms)
-        2-tuple containing arithmetic terms representing each term of the Jacobian.
+        2-tuple containing arithmetic terms representing each term of the scalar product of gradients.
     """
 
     uc = coordinate_system.coordinates[0]
@@ -59,29 +59,30 @@ def Jacobian(field1, field2, coordinate_system, sign=1, prefactors=(None, None))
         prefactor1 = prefactors[0]
         prefactor2 = prefactors[1]
     else:
-        prefs = 1 / (uc.infinitesimal_length * vc.infinitesimal_length)
+        prefs = 1 / (uc.infinitesimal_length ** 2)
         if prefactors[0] is not None:
             prefs1 = prefactors[0].symbol * prefs
             prefactor1 = Expression(prefs1,
                                     expression_parameters=coordinate_system.parameters,
-                                    units=combine_units(prefactors[0].units, combine_units(uc.units, vc.units, '+'), '-'),
+                                    units=combine_units(prefactors[0].units, power_units(uc.units, 2), '-'),
                                     latex=prefs1._repr_latex_()[15:-1])
         else:
             prefactor1 = Expression(prefs,
                                     expression_parameters=coordinate_system.parameters,
-                                    units=power_units(combine_units(uc.units, vc.units, '+'), -1),
+                                    units=power_units(uc.units, -2),
                                     latex=prefs._repr_latex_()[15:-1])
 
+        prefs = 1 / (vc.infinitesimal_length ** 2)
         if prefactors[1] is not None:
             prefs2 = prefactors[1].symbol * prefs
             prefactor2 = Expression(prefs2,
                                     expression_parameters=coordinate_system.parameters,
-                                    units=combine_units(prefactors[1].units, combine_units(uc.units, vc.units, '+'), '-'),
+                                    units=combine_units(prefactors[1].units, power_units(vc.units, 2), '-'),
                                     latex=prefs2._repr_latex_()[15:-1])
         else:
             prefactor2 = Expression(prefs,
                                     expression_parameters=coordinate_system.parameters,
-                                    units=power_units(combine_units(uc.units, vc.units, '+'), -1),
+                                    units=power_units(vc.units, -2),
                                     latex=prefs._repr_latex_()[15:-1])
 
     du_field1 = OperatorTerm(field1, D, u, prefactor=prefactor1)
@@ -90,19 +91,19 @@ def Jacobian(field1, field2, coordinate_system, sign=1, prefactors=(None, None))
     dv_field1 = OperatorTerm(field1, D, v, prefactor=prefactor2)
     dv_field2 = OperatorTerm(field2, D, v)
 
-    jacobian1 = ProductOfTerms(du_field1, dv_field2, sign=sign)
-    jacobian2 = ProductOfTerms(dv_field1, du_field2, sign=-sign)
+    product1 = ProductOfTerms(du_field1, du_field2, sign=sign)
+    product2 = ProductOfTerms(dv_field1, dv_field2, sign=sign)
 
-    return jacobian1, jacobian2
+    return product1, product2
 
 
-def vorticity_advection(field1, field2, coordinate_system, sign=1, prefactors=(None, None)):
+def vorticity_gradients_product(field1, field2, coordinate_system, sign=1, prefactors=(None, None)):
     """Function returning a list of :class:`~layercake.arithmetic.terms.operations.ProductOfTerms` components of
     the vorticity advection in the partial differential equation,
-    provided by the expression :math:`J(\\psi, \\nabla^2 \\phi)`,
-    where :math:`J` is the partial differential equation's Jacobian :func:`~layercake.arithmetic.terms.jacobian.Jacobian`:
+    provided by the expression :math:`P(\\psi, \\nabla^2 \\phi)`,
+    where :math:`P` is the partial differential equation's scalar product of gradients :func:`~layercake.arithmetic.terms.gradient.gradients_product`:
 
-    .. math:: J(\\psi, \\nabla^2 \\phi) = e_1 \\, e_2 \\, \\left(\\partial_{u_1} \\psi \\, \\partial_{u_2} \\nabla^2 \\phi - \\partial_{u_1} \\nabla^2 \\phi \\, \\partial_{u_2} \\psi \\right)
+    .. math:: P(\\psi, \\nabla^2 \\phi) = e_1^2 \\, \\partial_{u_1} \\psi \\, \\partial_{u_1} \\nabla^2 \\phi + e_2^2 \\, \\partial_{u_2} \\psi \\, \\partial_{u_2} \\nabla^2 \\phi
 
     where :math:`\\phi` and :math:`\\psi` are two fields defined on the model's domain,
     :math:`u_1, u_2` are the coordinates of the model, and :math:`e_1, e_2` are the inverse of the infinitesimal
@@ -140,29 +141,30 @@ def vorticity_advection(field1, field2, coordinate_system, sign=1, prefactors=(N
         prefactor1 = prefactors[0]
         prefactor2 = prefactors[1]
     else:
-        prefs = 1 / (uc.infinitesimal_length * vc.infinitesimal_length)
+        prefs = 1 / (uc.infinitesimal_length ** 2)
         if prefactors[0] is not None:
             prefs1 = prefactors[0].symbol * prefs
             prefactor1 = Expression(prefs1,
                                     expression_parameters=coordinate_system.parameters,
-                                    units=combine_units(prefactors[0].units, combine_units(uc.units, vc.units, '+'), '-'),
+                                    units=combine_units(prefactors[0].units, power_units(uc.units, 2), '-'),
                                     latex=prefs1._repr_latex_()[15:-1])
         else:
             prefactor1 = Expression(prefs,
                                     expression_parameters=coordinate_system.parameters,
-                                    units=power_units(combine_units(uc.units, vc.units, '+'), -1),
+                                    units=power_units(uc.units, -2),
                                     latex=prefs._repr_latex_()[15:-1])
 
+        prefs = 1 / (vc.infinitesimal_length ** 2)
         if prefactors[1] is not None:
             prefs2 = prefactors[1].symbol * prefs
             prefactor2 = Expression(prefs2,
                                     expression_parameters=coordinate_system.parameters,
-                                    units=combine_units(prefactors[1].units, combine_units(uc.units, vc.units, '+'), '-'),
+                                    units=combine_units(prefactors[1].units, power_units(vc.units, 2), '-'),
                                     latex=prefs2._repr_latex_()[15:-1])
         else:
             prefactor2 = Expression(prefs,
                                     expression_parameters=coordinate_system.parameters,
-                                    units=power_units(combine_units(uc.units, vc.units, '+'), -1),
+                                    units=power_units(vc.units, -2),
                                     latex=prefs._repr_latex_()[15:-1])
 
     du_field1 = OperatorTerm(field1, D, u, prefactor=prefactor1)
@@ -171,7 +173,7 @@ def vorticity_advection(field1, field2, coordinate_system, sign=1, prefactors=(N
     du_lap_field2 = ComposedOperatorsTerm(field2, (D, Laplacian), (u, coordinate_system))
     dv_lap_field2 = ComposedOperatorsTerm(field2, (D, Laplacian), (v, coordinate_system))
 
-    jacobian1 = ProductOfTerms(du_field1, dv_lap_field2, sign=sign)
-    jacobian2 = ProductOfTerms(dv_field1, du_lap_field2, sign=-sign)
+    product1 = ProductOfTerms(du_field1, du_lap_field2, sign=sign)
+    product2 = ProductOfTerms(dv_field1, dv_lap_field2, sign=sign)
 
-    return jacobian1, jacobian2
+    return product1, product2
