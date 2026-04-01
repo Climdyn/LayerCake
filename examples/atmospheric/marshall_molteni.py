3817dabf7869fbbae7233df31c176f6e47d8555f
Jonathan Demaeyer <jodemaey@meteo.be>
2026-04-01 11:58:31 +0200

    Finished first layer

--- examples/atmospheric/marshall_molteni.py
+++ examples/atmospheric/marshall_molteni.py
@@ -13,6 +13,7 @@ else:
 
 # importing all that is needed to create the cake
 from layercake import *
+from layercake.arithmetic.terms.operators import OperatorTerm, ComposedOperatorsTerm
 
 # importing specific modules to create the model basis of functions
 from layercake.basis.spherical_harmonics import SphericalHarmonicsBasis
@@ -31,7 +32,7 @@ from layercake.inner_products.definition import StandardSymbolicInnerProductDefi
 ##########################
 
 # Average radius of Earth
-R = Parameter(6371e3, symbol=Symbol('R'), units='[m]')
+R_E = Parameter(6371e3, symbol=Symbol('R_E'), units='[m]')
 
 # Rossby deformation radius for the 200-500-hPa layer
 R1 = Parameter(700.e3, symbol=Symbol('R_1'), units='[m]')
@@ -59,7 +60,7 @@ tau_E = Parameter(3 * 24 * 3600, symbol=Symbol(u'τ_E'), units='[s]')
 ######################
 
 # Adimensional Earth sphere radius parameter
-Rp = Parameter(1., symbol=Symbol("R'"), units='')
+Rp = Parameter(1., symbol=Symbol("R"), units='')
 parameters = [Rp]
 
 basis = SphericalHarmonicsBasis(parameters, {'M': 4})
@@ -79,13 +80,23 @@ T = Parameter(1. / (2. * float(omega)), symbol=symbols('T'), units='[s]')
 #######################################
 
 # Inverse of the square of the rescaled Rossby radius
-R1d = Parameter(R**2 / R1**2, symbol=Symbol("R'_1^-2"), units='')
-R2d = Parameter(R**2 / R2**2, symbol=Symbol("R'_2^-2"), units='')
+R1d = Parameter(R_E ** 2 / R1 ** 2, symbol=Symbol("R'_1^-2"), units='', latex="R'_1^{-2}")
+R2d = Parameter(R_E ** 2 / R2 ** 2, symbol=Symbol("R'_2^-2"), units='', latex="R'_2^{-2}")
 
 # Rescaled timescales
-tau_Rp = Parameter(tau_R / T, symbol=Symbol(u'τ_R'), units='[s]')
-tau_Hp = Parameter(tau_H / T, symbol=Symbol(u'τ_H'), units='[s]')
-tau_Ep = Parameter(tau_E / T, symbol=Symbol(u'τ_E'), units='[s]')
+tau_Rp = Parameter(tau_R / T, symbol=Symbol(u"τ'_R"), units='')
+tau_Hp = Parameter(tau_H / T, symbol=Symbol(u"τ'_H"), units='')
+tau_Ep = Parameter(tau_E / T, symbol=Symbol(u"τ'_E"), units='')
+
+# Products
+a_R1 = Parameter(R1d / tau_Rp, symbol=Symbol('a_R1'), units='', latex='a_{R1}')
+a_H = Parameter((21 * 2) ** -4 / tau_Hp, symbol=Symbol("a_H"), units='')
+a_H1 = Parameter((21 * 2) ** -4 * R1d / tau_Hp, symbol=Symbol("a_H1"), units='', latex='a_{H1}')
+
+# Climatological forcings
+s1r = np.zeros(len(basis))
+s1r[0] = 0.1
+S1p = ParameterField("S1p", "S'_1", s1r, basis, inner_products_definition, latex="S'_1")
 
 
 # Defining the fields
@@ -116,11 +127,11 @@ lin_lhs11 = LinearTerm(psi_1, prefactor=R1d, sign=-1)
 lin_lhs12 = LinearTerm(psi_2, prefactor=R1d)
 psi1_equation = Equation(psi_1, lhs_terms=[psi1_vorticity, lin_lhs12, lin_lhs11])
 
-# Defining the advection term
+# defining the advection term
 psi1_advection_term = vorticity_advection(psi_1, psi_1, basis.coordinate_system, sign=-1)
 psi1_equation.add_rhs_terms(psi1_advection_term)
 
-# Defining the Jacobian term
+# defining the Jacobian term
 psi12_jacobian = Jacobian(psi_1, psi_2, basis.coordinate_system, prefactors=(R1d, R1d), sign=-1)
 psi1_equation.add_rhs_terms(psi12_jacobian)
 
@@ -128,10 +139,28 @@ psi1_equation.add_rhs_terms(psi12_jacobian)
 beta_term = OperatorTerm(psi_1, D, llambda, sign=-1)
 psi1_equation.add_rhs_term(beta_term)
 
-# linear terms
-a12 = Parameter()
-lin_rhs11 = LinearTerm(psi_1, prefactor=R1d, sign=-1)
-lin_rhs12 = LinearTerm(psi_2, prefactor=R1d)
+# radiative linear terms (could reuse LHS ones in a second pass)
+lin_rad_rhs11 = LinearTerm(psi_1, prefactor=a_R1)
+lin_rad_rhs12 = LinearTerm(psi_2, prefactor=a_R1, sign=-1)
+psi1_equation.add_rhs_terms([lin_rad_rhs11, lin_rad_rhs12])
+
+# horizontal PV diffusion terms
+operators = (Laplacian,) * 10
+operators_args = (basis.coordinate_system,) * 10
+psi1_hdiffv = ComposedOperatorsTerm(psi_1, operators, operators_args, prefactor=a_H, sign=-1)
+psi1_equation.add_rhs_term(psi1_hdiffv)
+
+operators = (Laplacian,) * 8
+operators_args = (basis.coordinate_system,) * 8
+psi1_hdiff1 = ComposedOperatorsTerm(psi_1, operators, operators_args, prefactor=a_H1)
+psi1_equation.add_rhs_term(psi1_hdiff1)
+
+psi1_hdiff2 = ComposedOperatorsTerm(psi_2, operators, operators_args, prefactor=a_H1, sign=-1)
+psi1_equation.add_rhs_term(psi1_hdiff2)
+
+# climatological forcing
+psi1_forcing = LinearTerm(S1p)
+psi1_equation.add_rhs_term(psi1_forcing)
 
 
 # --------------------------------
@@ -158,20 +187,20 @@ cake.add_layer(layer1)
 #   Computing the tendencies
 #
 # --------------------------------
-
-# computing the tensor
-cake.compute_tensor(True, True)
-
-# computing the tendencies
-f, Df = cake.compute_tendencies()
-
-# integrating
-ic = np.random.rand(cake.ndim) * 0.1
-res = solve_ivp(f, (0., 10000000.), ic)
-
-ic = res.y[:, -1]
-res = solve_ivp(f, (0., 1000000.), ic)
-
-# plotting
-plt.plot(res.y[21], res.y[29])
-plt.show()
+#
+# # computing the tensor
+# cake.compute_tensor(True, True)
+#
+# # computing the tendencies
+# f, Df = cake.compute_tendencies()
+#
+# # integrating
+# ic = np.random.rand(cake.ndim) * 0.1
+# res = solve_ivp(f, (0., 10000000.), ic)
+#
+# ic = res.y[:, -1]
+# res = solve_ivp(f, (0., 1000000.), ic)
+#
+# # plotting
+# plt.plot(res.y[21], res.y[29])
+# plt.show()
