71753321a06eacc278850714080cc484837b5dd9
Jonathan Demaeyer <jodemaey@meteo.be>
2026-04-10 12:55:25 +0200

    Fixing some missing class in * imports

--- layercake/__init__.py
+++ layercake/__init__.py
@@ -3,13 +3,12 @@ from .variables.parameter import Parameter
 from .variables.field import Field, ParameterField, FunctionField
 from .basis.base import SymbolicBasis
 from .arithmetic.symbolic.expressions import Expression
-from .arithmetic.terms import (vorticity_advection, Jacobian, OperatorTerm,
-                               AdditionOfTerms, LinearTerm, ConstantTerm)
+from .arithmetic.terms import *
 from .arithmetic.equation import Equation
 from .arithmetic.symbolic.operators import Laplacian, D
 from .bakery.layers import Layer
 from .bakery.cake import Cake
 
 __all__ = ['Parameter', 'ParameterField', 'FunctionField', 'Field', 'Expression', 'SymbolicBasis', 'vorticity_advection', 'Jacobian',
-           'OperatorTerm', 'AdditionOfTerms', 'LinearTerm', 'ConstantTerm', 'Equation', 'Laplacian', 'D',
+           'OperatorTerm', 'ProductOfTerms', 'AdditionOfTerms', 'LinearTerm', 'ConstantTerm', 'Equation', 'Laplacian', 'D',
            'Layer', 'Cake']
