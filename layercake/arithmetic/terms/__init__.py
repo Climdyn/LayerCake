5fe4976f364c135dd24903d60062d6f6be9383ce
Jonathan Demaeyer <jodemaey@meteo.be>
2026-04-11 12:10:28 +0200

    Fixing some missing classes in * import (#42)
    * Fixing some missing class in * imports
    
    * Fixing checks when PR points to main branch

--- layercake/arithmetic/terms/__init__.py
+++ layercake/arithmetic/terms/__init__.py
@@ -1,9 +1,9 @@
 
 from .jacobian import vorticity_advection, Jacobian
 from .operators import OperatorTerm
-from .operations import AdditionOfTerms
+from .operations import AdditionOfTerms, ProductOfTerms
 from .linear import LinearTerm
 from .constant import ConstantTerm
 
-__all__ = ['vorticity_advection', 'Jacobian',
+__all__ = ['vorticity_advection', 'Jacobian', 'ProductOfTerms',
            'OperatorTerm', 'AdditionOfTerms', 'LinearTerm', 'ConstantTerm']
