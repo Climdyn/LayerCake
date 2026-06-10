a62525c9bf8d9ff95393d52d244e9e6d7417a840
Jonathan Demaeyer <jodemaey@meteo.be>
2026-06-10 22:18:48 +0200

    Added a block inversion algorithm for numerical case

--- layercake/utils/matrix.py
+++ layercake/utils/matrix.py
@@ -9,67 +9,105 @@
 """
 import warnings
 
+import numpy as np
+from numpy.linalg import LinAlgError
+
 from sympy import MutableSparseMatrix
 from sympy.matrices.exceptions import NonInvertibleMatrixError
 
 def block_matrix_inverse(P, blocks_extent, simplify=True):
-    """Function to invert a symbolic matrix :math:`P` divided by blocks.
+    """Function to invert a symbolic or numeric matrix :math:`P` divided by blocks.
 
     Parameters
     ----------
-    P: ~sympy.matrices.immutable.ImmutableSparseMatrix or ~sympy.matrices.mutable.MutableSparseMatrix
+    P: ~sympy.matrices.immutable.ImmutableSparseMatrix or ~sympy.matrices.mutable.MutableSparseMatrix or ~numpy.ndarray
         The block matrix to invert.
     blocks_extent: list(tuple)
         The extent of each block, as a list of 2-tuple.
     simplify: bool, optional
         Try to simplify the inverse.
+        Only applies to symbolic matrices.
         Default to `True`.
 
     Warnings
     --------
-    If the `simplify` argument is set to `False`, this function will not check if the
+    In the symbolic case, if the `simplify` argument is set to `False`, this function will not check if the
     matrix :math:`P` is invertible !
 
     """
-    if simplify:
-        if P.det().simplify() == 0:
-            raise NonInvertibleMatrixError('block_matrix_inverse: The provided matrix is not invertible.')
-    else:
-        warnings.warn(f'Inverting a symbolic matrix without checking that it is invertible. '
-                      'Be cautious about the result.')
-    be = blocks_extent.copy()
-    PP = P
-    B_list = list()
-    C_list = list()
-    Am1_list = list()
-    while len(be) >= 2:
-        rest_block_extent = (be[1][0], be[-1][1])
-        Ainv, B, C, D, PPP = _block_matrix_inverse_2x2(PP, (be[0], rest_block_extent))
+    if isinstance(P, np.ndarray):
+        if np.linalg.det(P) == 0:
+            raise LinAlgError('block_matrix_inverse: The provided matrix is not invertible.')
+        be = blocks_extent.copy()
+        PP = P
+        B_list = list()
+        C_list = list()
+        Am1_list = list()
+        while len(be) >= 2:
+            rest_block_extent = (be[1][0], be[-1][1])
+            Ainv, B, C, D, PPP = _block_matrix_inverse_2x2_num(PP, (be[0], rest_block_extent))
 
-        B_list.append(B)
-        C_list.append(C)
-        Am1_list.append(Ainv)
+            B_list.append(B)
+            C_list.append(C)
+            Am1_list.append(Ainv)
 
-        PP = PPP
-        ret = be[0][1]
-        be = [(p[0]-ret, p[1]-ret) for p in be[1:]]
+            PP = PPP
+            ret = be[0][1]
+            be = [(p[0] - ret, p[1] - ret) for p in be[1:]]
 
-    PP = PP.adjugate() / PP.det()
-    for Am1, B, C in zip(Am1_list[::-1], B_list[::-1], C_list[::-1]):
-        Ashape = Am1.shape[0]
-        Pshape = PP.shape[0]
-        mat_shape = Ashape + Pshape
-        Pp1 = MutableSparseMatrix(mat_shape, mat_shape, {})
-        Pp1[slice(0, Ashape), slice(0, Ashape)] = Am1 + Am1 @ B @ PP @ C @ Am1
-        Pp1[slice(0, Ashape), slice(Ashape, mat_shape)] = - Am1 @ B @ PP
-        Pp1[slice(Ashape, mat_shape), slice(0, Ashape)] = - PP @ C @ Am1
-        Pp1[slice(Ashape, mat_shape), slice(Ashape, mat_shape)] = PP
-        PP = Pp1
+        PP = np.linalg.inv(PP)
+        for Am1, B, C in zip(Am1_list[::-1], B_list[::-1], C_list[::-1]):
+            Ashape = Am1.shape[0]
+            Pshape = PP.shape[0]
+            mat_shape = Ashape + Pshape
+            Pp1 = np.zeros((mat_shape, mat_shape))
+            Pp1[slice(0, Ashape), slice(0, Ashape)] = Am1 + Am1 @ B @ PP @ C @ Am1
+            Pp1[slice(0, Ashape), slice(Ashape, mat_shape)] = - Am1 @ B @ PP
+            Pp1[slice(Ashape, mat_shape), slice(0, Ashape)] = - PP @ C @ Am1
+            Pp1[slice(Ashape, mat_shape), slice(Ashape, mat_shape)] = PP
+            PP = Pp1
 
-    if simplify:
-        return PP.simplify()
-    else:
         return PP
+    else:
+        if simplify:
+            if P.det().simplify() == 0:
+                raise NonInvertibleMatrixError('block_matrix_inverse: The provided matrix is not invertible.')
+        else:
+            warnings.warn(f'Inverting a symbolic matrix without checking that it is invertible. '
+                          'Be cautious about the result.')
+        be = blocks_extent.copy()
+        PP = P
+        B_list = list()
+        C_list = list()
+        Am1_list = list()
+        while len(be) >= 2:
+            rest_block_extent = (be[1][0], be[-1][1])
+            Ainv, B, C, D, PPP = _block_matrix_inverse_2x2(PP, (be[0], rest_block_extent))
+
+            B_list.append(B)
+            C_list.append(C)
+            Am1_list.append(Ainv)
+
+            PP = PPP
+            ret = be[0][1]
+            be = [(p[0]-ret, p[1]-ret) for p in be[1:]]
+
+        PP = PP.adjugate() / PP.det()
+        for Am1, B, C in zip(Am1_list[::-1], B_list[::-1], C_list[::-1]):
+            Ashape = Am1.shape[0]
+            Pshape = PP.shape[0]
+            mat_shape = Ashape + Pshape
+            Pp1 = MutableSparseMatrix(mat_shape, mat_shape, {})
+            Pp1[slice(0, Ashape), slice(0, Ashape)] = Am1 + Am1 @ B @ PP @ C @ Am1
+            Pp1[slice(0, Ashape), slice(Ashape, mat_shape)] = - Am1 @ B @ PP
+            Pp1[slice(Ashape, mat_shape), slice(0, Ashape)] = - PP @ C @ Am1
+            Pp1[slice(Ashape, mat_shape), slice(Ashape, mat_shape)] = PP
+            PP = Pp1
+
+        if simplify:
+            return PP.simplify()
+        else:
+            return PP
 
 def _block_matrix_inverse_2x2(P, blocks_extent):
     be = blocks_extent
@@ -78,6 +116,16 @@ def _block_matrix_inverse_2x2(P, blocks_extent):
     C = P[slice(*be[1]), slice(*be[0])]
     D = P[slice(*be[1]), slice(*be[1])]
     Ainv = A.adjugate()/A.det()
+
+    return Ainv, B, C, D, D - C @ Ainv @ B
+
+def _block_matrix_inverse_2x2_num(P, blocks_extent):
+    be = blocks_extent
+    A = P[slice(*be[0]), slice(*be[0])]
+    B = P[slice(*be[0]), slice(*be[1])]
+    C = P[slice(*be[1]), slice(*be[0])]
+    D = P[slice(*be[1]), slice(*be[1])]
+    Ainv = np.linalg.inv(A)
 
     return Ainv, B, C, D, D - C @ Ainv @ B
 
@@ -101,4 +149,23 @@ if __name__ == '__main__':
     G[6:, 6:] = D
     bl = [(0, 3), (3, 6), (6, 9)]
     Ginv = block_matrix_inverse(G, bl)
+
+    A = np.zeros((3, 3))
+    A[0,0] = 1
+    A[1,1] = 3
+    A[2,2] = 2
+    C = A /2
+    D = 3 * A
+    G =  np.zeros((9, 9))
+    G[:3, :3] = A
+    G[:3, 3:6] = -C
+    G[:3, 6:] = D
+    G[3:6, :3] = C
+    G[3:6, 3:6] = C
+    G[3:6, 6:] = - D
+    G[6:, :3] = - C
+    G[6:, 3:6] = A
+    G[6:, 6:] = D
+    bl = [(0, 3), (3, 6), (6, 9)]
+    Ginv_num = block_matrix_inverse(G, bl)
 
